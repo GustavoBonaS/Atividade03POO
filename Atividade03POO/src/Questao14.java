27af69b0e23322b3b25891b55d99342200f2b2cd
Gustavo Bona <gustavobosiqueira@gmail.com>
2021-10-14 20:26:18 -0300

    att03

--- Atividade03POO/src/Questao14.java
+++ Atividade03POO/src/Questao14.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Questao14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner ent = new Scanner(System.in);
+		
+		System.out.println("Insira um numero: ");
+		int cont = ent.nextInt();
+		
+		int num1 = 1;
+		int num2 = 1;
+		
+		for (int i = 0; i < cont; i++) {
+			System.out.print(+num1+ "/"+num2 + " + ");
+			num1++;
+			num2 = num2 + 2;
+		}
+		
+		ent.close();
 	}
 
 }
